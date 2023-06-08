309ab4897fd4ba7798c70ace042910a92477964c
bestnoob69 <110734669+bestnoob69@users.noreply.github.com>
2023-06-08 23:17:51 +0530

    Find_Consecutive_Sequence.cpp

--- Find_Consecutive_Sequence.cpp
+++ Find_Consecutive_Sequence.cpp
@@ -1,5 +1,8 @@
 #include <bits/stdc++.h>
 // Simple sorting and checking adjacents.
+// APPROACH 1
+// TC : O(nlogn)
+// SC : O(1)
 int lengthOfLongestConsecutiveSequence(vector<int> &arr, int n) {
     
     sort(arr.begin(),arr.end());
@@ -19,3 +22,33 @@ int lengthOfLongestConsecutiveSequence(vector<int> &arr, int n) {
     maxi = max(maxi,curr);
     return maxi;
 }// [20,30,33,34,35]
+
+// APPROACH 2
+// Check previous in set. If it's not there then it means that that particular element is the first in it's sequence.
+// Hence we now check it's next consecutive elements in the set.
+// It is unordered so time complexity is O(n) with O(n) space.
+#include <bits/stdc++.h>
+
+int lengthOfLongestConsecutiveSequence(vector<int> &arr, int n) {
+    
+    unordered_set<int> st;
+    for(auto &i : arr)st.insert(i);
+    int maxi = 0;
+    int curr = 0;
+    for(auto it = st.begin();it != st.end();it++){
+        auto p = *it;
+        if(st.find(p - 1) == st.end()){
+            curr = 0;
+            while(st.find(p) != st.end()){
+                curr++;
+                p++;
+            }
+
+
+        }
+        maxi = max(maxi,curr);
+    }
+    return maxi;
+    
+
+}
