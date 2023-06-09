0ab3fd3d51e76b32496e787214f2663183cd58c4
bestnoob69 <110734669+bestnoob69@users.noreply.github.com>
2023-06-09 18:31:03 +0530

    2 pointers + map

---
+++ Longest_Substring_with_unique_characters.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h> 
+int uniqueSubstrings(string s)
+{
+    // So I saw striver video here. Basically 2 pointers + map.
+  // Update l to last occurence of r to check for the next continuous sequence.
+    unordered_map<char,int> m;
+    int maxi = 0;
+    int curr = 0;
+    int r = 0,l = 0;
+    int n = s.size();
+    // for(auto &i : s)m[i] =
+    for(int i= 0;i<n;i++){
+      
+
+        if(m.find(s[i]) != m.end())l = max(m[s[i]] + 1,l);
+        m[s[i]] = i;
+      
+      maxi = max(maxi,i-l + 1);
+    }
+    return maxi;
+
+}
