8bb5ef066d3ebb843e68d1ac2ac73d6b5eafd4cf
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-01-24 15:25:07 +0530

    Create Pseudo-Palindromic Paths in a Binary Tree.cpp

---
+++ Backtracking/Pseudo-Palindromic Paths in a Binary Tree.cpp
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/pseudo-palindromic-paths-in-a-binary-tree/description/?envType=daily-question&envId=2024-01-24
+// TC : O(n) 
+// SC : O(h) [Recursive stack space]
+// BitMasking approach
+class Solution {
+public:
+    int ans = 0;
+    void solve(TreeNode* root, int result) {
+        if (root == NULL)
+            return;
+        int val = root->val;
+      // Storing the frequency as a bitmask.
+      // Even is 0 and odd is 1.
+        int newFrequency = result ^ (1 << val);
+    
+        if (root->left == NULL && root->right == NULL) {
+          // The result stores frequency of each value (1 - 9). So we expect result to have either 0 or 1 set bits.
+          // Hence we do n*n-1 operation as for 1 set bit it will always be 0
+            if ((newFrequency & (newFrequency - 1)) == 0) {
+                ans++;
+            }
+        }
+        solve(root->left, newFrequency);
+        solve(root->right, newFrequency);
+    }
+
+    int pseudoPalindromicPaths(TreeNode* root) {
+        solve(root, 0);
+        return ans;
+    }
+};
