78e725bd5474d307403f210e38ede73f4f5e767e
sonitmehr <110734669+sonitmehr@users.noreply.github.com>
2024-02-19 14:49:17 +0530

    Update Partition Array for Maximum Sum.cpp

--- DP/Partition Array for Maximum Sum.cpp
+++ DP/Partition Array for Maximum Sum.cpp
@@ -31,3 +31,28 @@ public:
        return solve(0,arr,k);
     }
 };
+
+// TABULATION APPROACH
+
+
+class Solution {
+public:
+    
+
+    int maxSumAfterPartitioning(vector<int>& arr, int k) {
+       
+        int n = arr.size();
+        vector<int> dp(n+1,0);
+
+        for(int i = n - 1;i>=0;i--){
+            int maxi = 0,ans = 0;
+            for(int j = i;j<(i + k) && j < n;j ++) {
+                maxi = max(maxi,arr[j]);
+                ans = max(ans, maxi * (j - i + 1) + dp[j + 1]);
+            }
+            dp[i] = ans;
+        }
+        return dp[0];
+
+    }
+};
